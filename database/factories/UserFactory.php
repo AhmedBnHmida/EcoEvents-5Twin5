b152e13c8708a563ba15bfff7f79ab42e89a4c78
ahmed benhmida <benhmida.ahmed1@esprit.tn>
2025-10-18 13:49:41 +0100

    fix: correct UserFactory to use ->faker instead of fake()

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'first_name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(), // Changed fake() to $this->faker->
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'role' => 'participant',
         ];
